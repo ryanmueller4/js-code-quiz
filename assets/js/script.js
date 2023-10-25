3a7837fd81a4ffb1c794f1ae6f9eb19fd38077e0
unknown <sixstrings22@outlook.com>
2023-10-25 15:15:31 -0500

    finished starting screen

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var startBtn = document.querySelector("#startBtn");
+
+function startQuiz() {
+    startBtn.setAttribute("class", "hide");
+    console.log("test")
+}
+
+//startBtn.addEventListener("click", startQuiz())
