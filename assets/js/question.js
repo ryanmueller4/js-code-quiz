3ad6cdd070d59076f969caad7eddd3668ba06888
unknown <sixstrings22@outlook.com>
2023-11-07 19:14:06 -0600

    added javascript and made js file for question array

---
+++ assets/js/question.js
@@ -0,0 +1,21 @@
+var questions = [{
+    question: "",
+    choices: ["c1", "c2", "c3", "c4"],
+    answer: 0,
+}, {
+    question: "q2",
+    choices: ["c1", "c2", "c3", "c4"],
+    answer: 3,
+}, {
+    question: "q3",
+    choices: ["c1", "c2", "c3", "c4"],
+    answer: 2,
+}, {
+    question: "q4",
+    choices: ["c1", "c2", "c3", "c4"],
+    answer: 3,
+}, {
+    question: "q5",
+    choices: ["c1", "c2", "c3", "c4"],
+    answer: 1,
+}]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,55 @@
 var startBtn = document.querySelector("#startBtn");
+var stage = document.querySelector(".stage")
+var questionDiv = document.querySelector(".questionDiv")
+var questionSection = document.querySelector("#questionSection")
+var index = 0
+var time = document.querySelector("#time")
+var setIntervalID = ""
+var timeLeft = questions.length * 15
 
 function startQuiz() {
     startBtn.setAttribute("class", "hide");
-    console.log("test")
+    questionSection.classList.remove("hide")
+    displayQuestion()
+    setIntervalID = setInterval(countdown, 1000)
 }
 
-//startBtn.addEventListener("click", startQuiz())
+function countdown() {
+    if (timeLeft >= 0) {
+        time.textContent = timeLeft--
+    }
+}
+
+function nextQuestion(event) {
+    var userChoice = event.target.getAttribute("data-id")
+    var answer = questions[index].answer
+    var message = document.querySelector("#message")
+    console.log(userChoice)
+    console.log(answer)
+    if (userChoice == answer) {
+        message.textContent = "Correct"
+    } else {
+        message.textContent = "Wrong"
+        timeLeft = timeLeft-10
+    }
+   index++
+   displayQuestion()
+}
+
+function displayQuestion() {
+    questionSection.innerHTML = `
+    <p>${questions[index].question}</p>
+                <ol class="choiceBtn">
+                    <li><button data-id="0">${questions[index].choices[0]}</button></li>
+                    <li><button data-id="1">${questions[index].choices[1]}</button></li>
+                    <li><button data-id="2">${questions[index].choices[2]}</button></li>
+                    <li><button data-id="3">${questions[index].choices[3]}</button></li>
+                </ol>
+    `
+    var choiceBtn = document.querySelector(".choiceBtn")
+    choiceBtn.addEventListener("click", nextQuestion)
+
+
+}
+
+startBtn.addEventListener("click", startQuiz)
